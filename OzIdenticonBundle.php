bcffbdc2c1d9f98627bf275ce60133a4c28edbe6
Sydney-o9 <anthony.rey21@gmail.com>
2014-01-14 20:26:21 +1100

    Initialized bundle

---
+++ OzIdenticonBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the OzIdenticonBundle package.
+ *
+ * (c) Sydney-o9
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Oz\IdenticonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * OzIdenticonBundle
+ *
+ * @author Sydney-o9
+ */
+class OzIdenticonBundle extends Bundle
+{
+}
